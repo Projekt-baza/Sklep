b7cbf6ee5e230ea557213eb6099d971069502851
xjaszczomb <73691041+xjaszczomb@users.noreply.github.com>
2020-10-30 22:20:34 +0100

    Add files via upload
    .

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+  <title>Sklep internetowy xyz</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+
+  <header>
+  <nav class="navbar-stylemodify navbar-fixed-top" data-offset-top="200"  >
+	  <div class="container">
+	  	<div class="row">
+
+		    <div class="navbar-header">
+		     <a class="navbar-logo" href=""><img src="" class=" logo" alt=""></a>
+		    </div>
+
+		    <div class="navbar-div-przyciski">
+		      <ul class="nav navbar-nav navbar-right navbar-links-custom">
+		        <li><a href="#">O nas</a></li>
+		          <li><a href="#">Pomoc</a></li>
+		        <li><a href="#">Kontakt</a></li>
+		      </ul>
+		    </div>
+
+	  	</div>
+	  </div>
+	</nav>
+</header>
+
+
+
+</body>
+</html>
