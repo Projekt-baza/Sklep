a91e732241192635d25ad8db05d4fbd1870bd3ba
Bartox0 <64317766+Bartox0@users.noreply.github.com>
2020-10-31 15:30:21 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,18 +3,17 @@
   <title>Sklep internetowy xyz</title>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 
 
-  <header>
   <nav class="navbar-stylemodify navbar-fixed-top" data-offset-top="200"  >
 	  <div class="container">
 	  	<div class="row">
 
 		    <div class="navbar-header">
-		     <a class="navbar-logo" href=""><img src="" class=" logo" alt=""></a>
+		     <a class="navbar-logo" href="img/logo.png"><img src="img/logo.png" class=" logo" alt=""></a>
 		    </div>
 
 		    <div class="navbar-div-przyciski">
@@ -22,13 +21,16 @@
 		        <li><a href="#">O nas</a></li>
 		          <li><a href="#">Pomoc</a></li>
 		        <li><a href="#">Kontakt</a></li>
+            <li><a href="#">Zaloguj się</a></li>
+            <li>
+            <input class="searchinput" type="text" role="searchbox"> <input class="searchbutton" type="submit" value="Szukaj">
+            </li>
 		      </ul>
 		    </div>
 
 	  	</div>
 	  </div>
 	</nav>
-</header>
 
 
 
