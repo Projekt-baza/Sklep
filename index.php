6db41fea11ceeafc768aff89b67be98d9deaeed1
Bartox0 <64317766+Bartox0@users.noreply.github.com>
2020-11-19 13:08:38 +0100

    Revert "Delete index.php"

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<head>
+  <title>Sklep internetowy xyz</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+  <nav class="navbar-stylemodify navbar-fixed-top" data-offset-top="200"  >
+	  <div class="container">
+	  	<div class="row">
+
+		    <div class="navbar-header">
+		     <a class="navbar-logo" href="img/logo.png"><img src="img/logo.png" class=" logo" alt=""></a>
+		    </div>
+
+		    <div class="navbar-div-przyciski">
+		      <ul class="nav navbar-nav navbar-right navbar-links-custom">
+		        <li><a href="#">O nas</a></li>
+		          <li><a href="#">Pomoc</a></li>
+		        <li><a href="#">Kontakt</a></li>
+            <li><a href="#">Zaloguj się</a></li>
+            <li>
+            <input class="searchinput" type="text" role="searchbox"> <input class="searchbutton" type="submit" value="Szukaj">
+            </li>
+		      </ul>
+		    </div>
+
+	  	</div>
+	  </div>
+	</nav>
+
+
+
+</body>
+</html>
