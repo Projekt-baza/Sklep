7986f43757f703423ad703a5a8d6b10a0bdb2f44
Radeven <pikusik222@gmail.com>
2020-11-23 13:27:36 +0100

    Update index.php

--- STORE/index.php
+++ STORE/index.php
@@ -83,79 +83,23 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
                             <div class="sb-sidenav-menu-heading">Kategorie</div>
-
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-mars"></i></div>
-                                Dla mężczyzn
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"></div>
+                                Bluzy
                             </a>
-
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
-                              <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                      Podstrona1
-                                      <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                  </a>
-                                  <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-                                      <nav class="sb-sidenav-menu-nested nav">
-                                          <a class="nav-link" href="#">Lista1</a>
-                                          <a class="nav-link" href="#">Lista2</a>
-                                          <a class="nav-link" href="#">Lista3</a>
-                                      </nav>
-                                  </div>
-
-
-                                  <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                      Podstrona2
-                                      <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                  </a>
-                                  <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-                                      <nav class="sb-sidenav-menu-nested nav">
-                                          <a class="nav-link" href="#">Lista1</a>
-                                          <a class="nav-link" href="#">Lista2</a>
-                                          <a class="nav-link" href="#">Lista3</a>
-                                      </nav>
-                                  </div>
-                              </nav>
-                          </div>
-
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-venus"></i></div>
-                                Dla kobiet
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"></div>
+                                Spodnie
                             </a>
-
-
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        Podstrona1
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="#">Lista1</a>
-                                            <a class="nav-link" href="#">Lista2</a>
-                                            <a class="nav-link" href="#">Lista3</a>
-                                        </nav>
-                                    </div>
-
-
-                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        Podstrona2
-                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                                    </a>
-                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
-                                        <nav class="sb-sidenav-menu-nested nav">
-                                            <a class="nav-link" href="#">Lista1</a>
-                                            <a class="nav-link" href="#">Lista2</a>
-                                            <a class="nav-link" href="#">Lista3</a>
-                                        </nav>
-                                    </div>
-                                </nav>
-                            </div>
-
-
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"></div>
+                                Buty
+                            </a>
+                            <a class="nav-link" href="#">
+                                <div class="sb-nav-link-icon"></div>
+                                T-shirt
+                            </a>
+                            
 
                             <div class="sb-sidenav-menu-heading">Informacje</div>
                             <a class="nav-link" href="#">
