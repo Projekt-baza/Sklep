1e69669cd41f0018552344c85436a52cc10cabd8
Bartox0 <64317766+Bartox0@users.noreply.github.com>
2020-11-30 13:01:53 +0100

    Update editaddress.php

--- STORE/adminpanel/editaddress.php
+++ STORE/adminpanel/editaddress.php
@@ -87,9 +87,6 @@
                                    <a class="nav-link collapsed" href="minusemployee.php">
                                          <div class="sb-nav-link-icon"><i class="fas fa-minus-circle"></i></div>Usuń
                                    </a>
-                                   <a class="nav-link collapsed" href="editemployee.php">
-                                         <div class="sb-nav-link-icon"><i class="fas fa-edit"></i></div>Edytuj
-                                   </a>
 
                                  </nav>
                              </div>
@@ -104,9 +101,6 @@
                                    </a>
                                    <a class="nav-link collapsed" href="minususer.php">
                                          <div class="sb-nav-link-icon"><i class="fas fa-minus-circle"></i></div>Usuń
-                                   </a>
-                                   <a class="nav-link collapsed" href="edituser.php">
-                                         <div class="sb-nav-link-icon"><i class="fas fa-edit"></i></div>Edytuj
                                    </a>
 
                                  </nav>
