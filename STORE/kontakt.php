4271a065e095401507c7f988079dc59e289659f8
Radeven <pikusik222@gmail.com>
2020-11-30 10:10:33 +0100

    Drobniutko

--- STORE/kontakt.php
+++ STORE/kontakt.php
@@ -114,11 +114,7 @@ $ka = $kat->fetchAll(PDO::FETCH_ASSOC);
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Jastrzębska Hepner Gastołek 2020</div>
-                            <div>
-                                <a href="#">Privacy Policy</a>
-                                &middot;
-                                <a href="#">Terms &amp; Conditions</a>
-                            </div>
+                          
                         </div>
                     </div>
                 </footer>
