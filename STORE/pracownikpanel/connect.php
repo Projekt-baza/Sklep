92920948ff60156cb5c343c1b56cf84845153a79
Radeven <pikusik222@gmail.com>
2020-11-30 14:31:04 +0100

    Polskie znaki teraz działają

--- STORE/pracownikpanel/connect.php
+++ STORE/pracownikpanel/connect.php
@@ -7,6 +7,7 @@
 
     try{
         $pdo = new PDO('mysql:host='.$host.';dbname='.$db,$user,$pass);
+        $pdo->exec("set names utf8");
     }catch(PDOException $e){
         echo "Blad Polaczenia";
         die();
